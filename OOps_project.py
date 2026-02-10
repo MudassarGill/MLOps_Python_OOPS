7c96ee350722ac7a3ab888818868f88c188d98d6
Mudassar Gill <mudassarhussain6533@gmail.com>
2026-02-10 02:31:37 -0800

    Add some more Functions here..

--- OOps_project.py
+++ OOps_project.py
@@ -5,10 +5,13 @@ class Chatbook:
         self.password = ""
         self.is_logged_in = False
     def menu(self):
-        print("Welcome to Chatbook")
+        print("Welcome to Chatbook")    
         print("1. Login")
         print("2. signup")
-        print("3. Exit")
+        print("3. forgot password")
+        print("4. write a post...")
+        print("5. Send message...")
+        print("6. Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -16,6 +19,12 @@ class Chatbook:
         elif choice == "2":
             self.signup()
         elif choice == "3":
+            self.forgot_password()
+        elif choice == "4":
+            self.write_post()
+        elif choice == "5":
+            self.send_message()
+        elif choice == "6":
             exit()
         else:
             print("Invalid choice")
@@ -34,6 +43,32 @@ class Chatbook:
             self.menu()
         else:
             print("Invalid username or password")
+            self.menu()
+    def forgot_password(self):
+        username = input("Enter your username: ")
+        if username == self.username:
+            self.password = input("Enter your new password: ")
+            print("Password changed successfully")
+            self.menu()
+        else:
+            print("Invalid username")
+            self.menu()
+    def write_post(self):
+        if self.is_logged_in==True:
+            post = input("Enter your post: ")
+            print("Post written successfully")
+            self.menu()
+        else:
+            print("Please login to write a post")
+            self.menu()
+    def send_message(self):
+        if self.is_logged_in==True:
+            message = input("Enter your message: ")
+            receiver = input("Enter receiver username: ")
+            print(f"Message sent to {receiver}")
+            self.menu()
+        else:
+            print("Please login to send a message")
             self.menu()
         
 obj = Chatbook()
