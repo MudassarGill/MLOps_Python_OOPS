01956ebee111915d0414fec322f157f146ad50d1
Mudassar Gill <mudassarhussain6533@gmail.com>
2026-02-10 01:03:04 -0800

    How our class and object work

---
+++ oop1.py
@@ -0,0 +1,14 @@
+# Here we write our class and object code
+class employee:
+    def __init__(self):# these called dunder method or constructor
+        self.name = "Mudassar"
+        self.age = 20
+        self.salary = 50000
+
+# now we create object
+emp1 = employee()
+print(emp1.name)
+print(emp1.age)
+print(emp1.salary)
+    
+    
